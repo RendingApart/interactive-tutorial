99e3153ab77ec0f88d296324534427d7325bae8f
RendingApart <133361185+RendingApart@users.noreply.github.com>
2023-05-31 18:16:15 +0000

    WM2 boken with webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { useEffect } from "react";
 import "./App.scss";
 import { Editor } from "@monaco-editor/react";
 import * as mocha from "./monaco-mocha.json";
 import palette from "@catppuccin/palette";
+// import { VM } from "vm2";
 
 function App() {
   return (
     <div className="App">
+      <p>Hello hi test hi</p>
       <div className="editor-container">
-      <Editor
-        height="50vh"
-        width="50vw"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
-        theme="catppuccinmocha"
-        loading={<span style={{width: "100%", height: "100%", position: "absolute", backgroundColor: palette.variants.mocha.base}}></span>}
-        beforeMount={(monaco) => {
-          monaco.editor.defineTheme("catppuccinmocha", mocha)
-        }}
-      />
+        <Editor
+          height="50vh"
+          width="50vw"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme="catppuccinmocha"
+          loading={
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                backgroundColor: palette.variants.mocha.mauve.hex,
+              }}
+            ></div>
+          }
+          beforeMount={(monaco) => {
+            monaco.editor.defineTheme("catppuccinmocha", mocha);
+          }}
+        />
       </div>
-
     </div>
   );
 }
